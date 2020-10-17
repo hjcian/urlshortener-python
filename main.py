4c21dd974f6bc978c57e247b5c13cee5eb0b2728
hjcian <chutarojian@gmail.com>
2020-10-17 14:30:47 +0800

    🔨 Refactor: Add basic error handling

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 from flask import Flask, Response, request
 
 from dbhandler import DBHandler
-from keygenerator import generate
+from tokengenerator import generate
 
 app = Flask(__name__)
 DB = DBHandler()
@@ -13,41 +13,51 @@ DB = DBHandler()
 
 @app.route('/shortenURL', methods=['POST'])
 def shortenURL():
-    req = request.get_json()
-    url = req.get("url")
+    try:
+        req = request.get_json()
+        url = req.get("url")
+        if not url or not isinstance(url, str):
+            return Response(status=400)
 
-    token = generate(url)
-    ret = DB.insertEntry(url=url, token=token)
-    if ret != DB.INSERT_OK:
-        print("entry collision, but return the token for accessing is OK")
+        token = generate(url)
+        ret = DB.insertEntry(url=url, token=token)
+        if ret != DB.INSERT_OK:
+            print("entry collision, but return the token for accessing is OK")
 
-    return Response(
-        response=json.dumps({
-            "url": url,
-            "token": token
-        }),
-        status=200,
-        mimetype="application/json"
-    )
+        return Response(
+            response=json.dumps({
+                "token": token
+            }),
+            status=200,
+            mimetype="application/json"
+        )
+    except Exception as e:
+        print("Unexpected error: {}".format(e))
+        return Response(status=500)
 
 
 @app.route('/getURL', methods=['POST'])
 def getURL():
-    req = request.get_json()
-    token = req.get("token")
+    try:
+        req = request.get_json()
+        token = req.get("token")
+        if not token or not isinstance(token, str):
+            return Response(status=400)
 
-    url = DB.getURL(token)
-    if not url:
-        return Response(status=404)
+        url = DB.getURL(token)
+        if not url:
+            return Response(status=404)
 
-    return Response(
-        response=json.dumps({
-            "url": url,
-            "token": token
-        }),
-        status=200,
-        mimetype="application/json"
-    )
+        return Response(
+            response=json.dumps({
+                "url": url
+            }),
+            status=200,
+            mimetype="application/json"
+        )
+    except Exception as e:
+        print("Unexpected error: {}".format(e))
+        return Response(status=500)
 
 
 if __name__ == "__main__":
