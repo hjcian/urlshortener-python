cddd8429a0bcedee5e4857127dc80ec8e2e02de3
hjcian <chutarojian@gmail.com>
2020-10-17 12:16:08 +0800

    🔨 Refactor: Use the abstraction layers to manipulate the DB and key generation for further developi

--- main.py
+++ main.py
@@ -1,54 +1,34 @@
-from flask import Flask, request
-import hashlib
-import string
+import json
+
+from flask import Flask, Response, request
+
+from dbhandler import DBHandler
+from keygenerator import generate
 
 app = Flask(__name__)
-
-CHARSET = string.digits + string.ascii_uppercase + string.ascii_lowercase
-BASE = len(CHARSET)
-CONSIDERED_BYTES = 5  # 40 bits of 128 bits (MD5)
-BYTE = 8  # 8 bits
-
-DB = {}
-
-
-def getInterger(url, apikey):
-    # "a" -> "xnddqK"
-    byte_array = hashlib.md5((url + apikey).encode()).digest()
-
-    big_num = sum([
-        byte_array[i] << ((CONSIDERED_BYTES - i - 1) * BYTE)
-        for i in range(CONSIDERED_BYTES)
-    ])
-
-    return big_num
-
-
-def encode(big_num):
-    token = ""
-
-    while big_num > 0:
-        big_num, x = divmod(big_num, BASE)
-        token = "{}{}".format(CHARSET[x], token)
-
-    return token
-
-
-def generateToken(url, apikey):
-    big_num = getInterger(url, apikey)
-    token = encode(big_num)
-    return token
+DB = DBHandler()
+# DB is a DB handler, can be an actual DB server's connection pool
+# by passing some configurations.
 
 
 @app.route('/shortenURL', methods=['POST'])
 def shortenURL():
     req = request.get_json()
     url = req.get("url")
-    apikey = req.get("apikey")
 
-    token = generateToken(url, apikey)
-    DB[token] = url
-    return "{}, {}".format(apikey, token), 200
+    token = generate(url)
+    ret = DB.insertEntry(url=url, token=token)
+    if ret != DB.INSERT_OK:
+        print("entry collision, but return the token for accessing is OK")
+
+    return Response(
+        response=json.dumps({
+            "url": url,
+            "token": token
+        }),
+        status=200,
+        mimetype="application/json"
+    )
 
 
 @app.route('/getURL', methods=['POST'])
@@ -56,8 +36,18 @@ def getURL():
     req = request.get_json()
     token = req.get("token")
 
-    url = DB.get(token)
-    return "{}: {}".format(token, url), 200
+    url = DB.getURL(token)
+    if not url:
+        return Response(status=404)
+
+    return Response(
+        response=json.dumps({
+            "url": url,
+            "token": token
+        }),
+        status=200,
+        mimetype="application/json"
+    )
 
 
 if __name__ == "__main__":
